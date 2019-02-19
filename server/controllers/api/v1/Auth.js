73a54e23a2047e3d27f9ce15166a0abdb07f52ee
dburgos <david.burgos.risco@gmail.com>
2019-02-19 18:30:08 +0100

    Fix memory leak on rejecting Auth 🔥

--- server/controllers/api/v1/Auth.js
+++ server/controllers/api/v1/Auth.js
@@ -1,34 +1,40 @@
 exports.validate = async (req, res, next) => {
-  await requireToken(req, res);
-  await checkToken(req, res);
+  try {
+    await requireToken(req, res);
+    await checkToken(req, res);
+  } catch (out) {
+    return res.status(out.status).json(out);
+  }
   next();
 };
 
 async function requireToken(req, res) {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const token = req.header('Authorization');
     const hasAuth = token && req.header('Authorization');
     if (!hasAuth) {
       const out = {
+        status: 400,
         code: 'INVALID_AUTH',
         message: 'Authorization header is required'
       };
-      return res.status(400).json(out);
+      return reject(out);
     }
     resolve();
   });
 }
 
 async function checkToken(req, res) {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const token = req.header('Authorization');
     const isValid = token === process.env.API_AUTH_TOKEN;
     if (!isValid) {
       const out = {
+        status: 401,
         code: 'NOT_AUTHORIZED',
         message: 'Authorization header is invalid'
       };
-      return res.status(401).json(out);
+      return reject(out);
     }
     resolve();
   });
